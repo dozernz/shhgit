78b764e611b3fd44f5e477da00defc394ebdee38
d0zer <dozernz@users.noreply.github.com>
2019-12-31 00:45:12 +0700

    Update match.go

--- core/match.go
+++ core/match.go
@@ -27,6 +27,42 @@ func NewMatchFile(path string) MatchFile {
 	}
 }
 
+
+func isBinary(path string) bool { 
+    f, err := os.Open(path)
+    if err != nil{
+        session.Log.Important("error opening path %s - %s",path,err)
+        return false
+    }
+    buffer := make([]byte, 512)
+
+    count, err := f.Read(buffer)
+    if err != nil {
+        session.Log.Important("error reading path %s",path)
+        return false
+    }
+    err = f.Close()
+    if err != nil {
+        session.Log.Important("error closing path %s",path)
+        return false
+    }
+
+    b := buffer[:count]
+
+    for _, s := range b {
+        if s == byte(0) {
+            return true
+        }
+    }
+
+
+    // Use the net/http package's handy DectectContentType function. Always returns a valid
+    // content-type by returning "application/octet-stream" if no others seemed to match.
+
+    return false
+}
+
+
 func IsSkippableFile(path string) bool {
 	extension := strings.ToLower(filepath.Ext(path))
 
@@ -35,6 +71,33 @@ func IsSkippableFile(path string) bool {
 			return true
 		}
 	}
+
+    if *session.Options.ExcludeDirs != ""{
+        if strings.Contains(path, *session.Options.ExcludeDirs){
+            session.Log.Debug("Excluding path because of  exclude-dirs  %s",path)
+            return true
+        }
+    }
+
+
+    //Dont check or follow symlinks
+    fi, err := os.Lstat(path)
+    if err != nil{
+         session.Log.Important("error lstatting path %s",path)
+    }
+    if fi.Mode() & os.ModeSymlink == os.ModeSymlink {
+        session.Log.Debug("Skipping symlink with path %s",path)
+        return true
+    }
+
+
+    if *session.Options.SkipBinaries == true{
+        if isBinary(path) {
+            session.Log.Debug("[DEBUG] Skipping binary file with path %s",path)
+            return true
+        }
+    }
+
 
 	for _, skippablePathIndicator := range session.Config.BlacklistedPaths {
 		skippablePathIndicator = strings.Replace(skippablePathIndicator, "{sep}", string(os.PathSeparator), -1)
@@ -63,15 +126,18 @@ func (match MatchFile) CanCheckEntropy() bool {
 func GetMatchingFiles(dir string) []MatchFile {
 	fileList := make([]MatchFile, 0)
 	maxFileSize := *session.Options.MaximumFileSize * 1024
+    count := 0
 
 	filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
-		if err != nil || f.IsDir() || uint(f.Size()) > maxFileSize || IsSkippableFile(path) {
+		if err != nil || f.IsDir() || uint(f.Size()) > maxFileSize || uint(f.Size()) == 0 || IsSkippableFile(path) {
 			return nil
 		}
-
+        session.Log.Debug("Adding file %s",path)
 		fileList = append(fileList, NewMatchFile(path))
+        count +=1 
 		return nil
 	})
 
+    session.Log.Important("added %d files",count)
 	return fileList
 }
