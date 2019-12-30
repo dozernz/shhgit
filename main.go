cf538779cc29d3bddff23b2d5fbdb0ad3603c399
d0zer <dozernz@users.noreply.github.com>
2019-12-30 19:18:36 +0700

    Update main.go

--- main.go
+++ main.go
@@ -133,6 +133,83 @@ func processRepositoryOrGist(url string) {
 	}
 }
 
+func processDir(path string) {
+	var (
+		matches    []string
+		matchedAny bool = false
+	)
+
+	//dir := core.GetTempDir(core.GetHash(url))
+	dir := core.GetAbs(path)
+	//_, err := core.CloneRepository(session, url, dir)
+
+
+	//session.Log.Debug("[%s] Cloning in to %s", path, strings.Replace(dir, *session.Options.TempDirectory, "", -1))
+
+	for _, file := range core.GetMatchingFiles(dir) {
+		relativeFileName := strings.Replace(file.Path, *session.Options.TempDirectory, "", -1)
+
+		if *session.Options.SearchQuery != "" {
+			queryRegex := regexp.MustCompile(*session.Options.SearchQuery)
+			for _, match := range queryRegex.FindAllSubmatch(file.Contents, -1) {
+				matches = append(matches, string(match[0]))
+			}
+
+			if matches != nil {
+				count := len(matches)
+				m := strings.Join(matches, ", ")
+				session.Log.Important("[%s] %d %s for %s in file %s: %s", path, count, core.Pluralize(count, "match", "matches"), color.GreenString("Search Query"), relativeFileName, color.YellowString(m))
+				session.WriteToCsv([]string{path, "Search Query", relativeFileName, m})
+			}
+		} else {
+			for _, signature := range session.Signatures {
+				if matched, part := signature.Match(file); matched {
+					matchedAny = true
+
+					if part == core.PartContents {
+						if matches = signature.GetContentsMatches(file); matches != nil {
+							count := len(matches)
+							m := strings.Join(matches, ", ")
+							session.Log.Important("[%s] %d %s for %s in file %s: %s", path, count, core.Pluralize(count, "match", "matches"), color.GreenString(signature.Name()), relativeFileName, color.YellowString(m))
+							session.WriteToCsv([]string{path, signature.Name(), relativeFileName, m})
+						}
+					} else {
+						if *session.Options.PathChecks {
+							session.Log.Important("[%s] Matching file %s for %s", path, color.YellowString(relativeFileName), color.GreenString(signature.Name()))
+							session.WriteToCsv([]string{path, signature.Name(), relativeFileName, ""})
+						}
+
+						if *session.Options.EntropyThreshold > 0 && file.CanCheckEntropy() {
+							scanner := bufio.NewScanner(bytes.NewReader(file.Contents))
+
+							for scanner.Scan() {
+								line := scanner.Text()
+
+								if len(line) > 6 && len(line) < 100 {
+									entropy := core.GetEntropy(scanner.Text())
+
+									if entropy >= *session.Options.EntropyThreshold {
+										session.Log.Important("[%s] Potential secret in %s = %s", path, color.YellowString(relativeFileName), color.GreenString(scanner.Text()))
+										session.WriteToCsv([]string{path, signature.Name(), relativeFileName, scanner.Text()})
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		if !matchedAny {
+			os.Remove(file.Path)
+		}
+	}
+
+	if !matchedAny {
+		os.RemoveAll(dir)
+	}
+}
+
 func main() {
 	session.Log.Info("%s v%s started. Loaded %d signatures. Using %d GitHub tokens and %d threads. Work dir: %s", core.Name, core.Version, len(session.Signatures), len(session.Clients), *session.Options.Threads, *session.Options.TempDirectory)
 
@@ -140,13 +217,21 @@ func main() {
 		session.Log.Important("Search Query '%s' given. Only returning matching results.", *session.Options.SearchQuery)
 	}
 
-	go core.GetRepositories(session)
-	go ProcessRepositories()
 
-	if *session.Options.ProcessGists {
-		go core.GetGists(session)
-		go ProcessGists()
-	}
+    if *session.Options.TargetDir != "" {
+        processDir(*session.Options.TargetDir)
+    } else{
+        session.Log.Info("Did not select a dir to process")
+    }
+
+	//go core.GetRepositories(session)
+	//go ProcessRepositories()
+
+
+	//if *session.Options.ProcessGists {
+	//	go core.GetGists(session)
+	//	go ProcessGists()
+	//}
 
 	session.Log.Info("Press Ctrl+C to stop and exit.\n")
 	select {}
