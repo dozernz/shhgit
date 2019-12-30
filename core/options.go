f77179c1d1315a54590cf021c2069fbbfe4958ac
d0zer <dozernz@users.noreply.github.com>
2019-12-31 00:44:25 +0700

    Update options.go

--- core/options.go
+++ core/options.go
@@ -17,9 +17,11 @@ type Options struct {
 	MinimumStars           *uint
 	PathChecks             *bool
 	ProcessGists           *bool
+	SkipBinaries           *bool
 	TempDirectory          *string
 	CsvPath                *string
 	SearchQuery            *string
+	ExcludeDirs            *string
 	TargetDir            *string
 }
 
@@ -35,9 +37,11 @@ func ParseOptions() (*Options, error) {
 		MinimumStars:           flag.Uint("minimum-stars", 0, "Only process repositories with this many stars. Default 0 will ignore star count"),
 		PathChecks:             flag.Bool("path-checks", true, "Set to false to disable checking of filepaths, i.e. just match regex patterns of file contents"),
 		ProcessGists:           flag.Bool("process-gists", true, "Will watch and process Gists. Set to false to disable."),
+		SkipBinaries:           flag.Bool("skip-binaries", true, "Skip searching through binary files (anything with the application/octet-stream filetype)"),
 		TempDirectory:          flag.String("temp-directory", filepath.Join(os.TempDir(), Name), "Directory to process and store repositories/matches"),
 		CsvPath:                flag.String("csv-path", "", "CSV file path to log found secrets to. Leave blank to disable"),
 		SearchQuery:            flag.String("search-query", "", "Specify a search string to ignore signatures and filter on files containing this string (regex compatible)"),
+		ExcludeDirs:            flag.String("exclude-dirs", "", "Does not add files if this string is in the path. Use like --exclude-dirs /usr/ for example"),
 		TargetDir:              flag.String("target-dir", "", "Directory to run the tool over"),
 	}
 
